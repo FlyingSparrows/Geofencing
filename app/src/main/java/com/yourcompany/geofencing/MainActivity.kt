27951a050ac365032239323e3e7a005aa24a4b7d
FlyingSparrows <tamarakijkindevegte@live.nl>
2022-10-12 14:01:31 +0200

    Initial commit

---
+++ app/src/main/java/com/yourcompany/geofencing/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.yourcompany.geofencing
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+//    private val gadgetQ = android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
